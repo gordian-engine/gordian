1e54b66248c769320689297e7e83d35deef2752e
Reece Williams <reecepbcups@gmail.com>
2024-08-02 20:03:09 -0500

    fix: TxDecoder as `transaction.Codec[transaction.Tx]`

--- gcosmos/gtx/tx_decode.go
+++ gcosmos/gtx/tx_decode.go
@@ -1,26 +1,58 @@
 package gtx
 
 import (
+	"errors"
+
 	"cosmossdk.io/core/transaction"
 	"github.com/cosmos/cosmos-sdk/client"
 )
 
-// txDecoder adapts client.TxConfig to the transaction.Codec type.
-// This is a copy of "temporarytxDecoder" from simapp code.
-type txDecoder struct {
-	TxConfig client.TxConfig
+var _ transaction.Codec[transaction.Tx] = &txDecoder[transaction.Tx]{}
+
+type txDecoder[T transaction.Tx] struct {
+	txConfig client.TxConfig
 }
 
-func NewTxDecoder(txConfig client.TxConfig) txDecoder {
-	return txDecoder{TxConfig: txConfig}
+func NewTxDecoder(txConfig client.TxConfig) txDecoder[transaction.Tx] {
+	if txConfig == nil {
+		panic("NewTxDecoder txConfig is nil")
+	}
+
+	return txDecoder[transaction.Tx]{
+		txConfig: txConfig,
+	}
 }
 
 // Decode implements transaction.Codec.
-func (t txDecoder) Decode(bz []byte) (transaction.Tx, error) {
-	return t.TxConfig.TxDecoder()(bz)
+func (t txDecoder[T]) Decode(bz []byte) (T, error) {
+	var out T
+	tx, err := t.txConfig.TxDecoder()(bz)
+	if err != nil {
+		return out, err
+	}
+
+	var ok bool
+	out, ok = tx.(T)
+	if !ok {
+		return out, errors.New("unexpected Tx type")
+	}
+
+	return out, nil
 }
 
 // DecodeJSON implements transaction.Codec.
-func (t txDecoder) DecodeJSON(bz []byte) (transaction.Tx, error) {
-	return t.TxConfig.TxJSONDecoder()(bz)
+func (t txDecoder[T]) DecodeJSON(bz []byte) (T, error) {
+	var out T
+	tx, err := t.txConfig.TxJSONDecoder()(bz)
+	if err != nil {
+		return out, err
+	}
+
+	var ok bool
+	out, ok = tx.(T)
+	if !ok {
+		return out, errors.New("unexpected Tx type")
+	}
+
+	return out, nil
 }
