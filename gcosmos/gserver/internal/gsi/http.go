f717753288ca69164fe7e8804adf38a0507f3e75
Reece Williams <reecepbcups@gmail.com>
2024-08-19 14:37:48 -0500

    feat: wrap gRPC client within http handler

--- gcosmos/gserver/internal/gsi/http.go
+++ gcosmos/gserver/internal/gsi/http.go
@@ -4,19 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log/slog"
 	"net"
 	"net/http"
 
-	"cosmossdk.io/core/transaction"
-	"cosmossdk.io/server/v2/appmanager"
-	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/gorilla/mux"
-	"github.com/rollchains/gordian/gcrypto"
-	"github.com/rollchains/gordian/gcosmos/gserver/internal/txmanager"
-	"github.com/rollchains/gordian/tm/tmp2p/tmlibp2p"
-	"github.com/rollchains/gordian/tm/tmstore"
+	"github.com/rollchains/gordian/gcosmos/gserver/internal/ggrpc"
 )
 
 type HTTPServer struct {
@@ -26,19 +19,7 @@ type HTTPServer struct {
 type HTTPServerConfig struct {
 	Listener net.Listener
 
-	FinalizationStore tmstore.FinalizationStore
-	MirrorStore       tmstore.MirrorStore
-
-	CryptoRegistry *gcrypto.Registry
-
-	Libp2pHost *tmlibp2p.Host
-	Libp2pconn *tmlibp2p.Connection
-
-	AppManager appmanager.AppManager[transaction.Tx]
-	TxCodec    transaction.Codec[transaction.Tx]
-	Codec      codec.Codec
-
-	TxBuffer *txmanager.SDKTxBuf
+	GordianGRPC ggrpc.GordianGRPCClient
 }
 
 func NewHTTPServer(ctx context.Context, log *slog.Logger, cfg HTTPServerConfig) *HTTPServer {
@@ -90,90 +71,79 @@ func newMux(log *slog.Logger, cfg HTTPServerConfig) http.Handler {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/blocks/watermark", handleBlocksWatermark(log, cfg)).Methods("GET")
-	r.HandleFunc("/validators", handleValidators(log, cfg)).Methods("GET")
+	// TODO:
+	// r.HandleFunc("/validators", handleValidators(log, cfg)).Methods("GET")
 
-	setDebugRoutes(log, cfg, r)
+	// setDebugRoutes(log, cfg, r)
 
-	setCompatRoutes(log, cfg, r)
+	// setCompatRoutes(log, cfg, r)
 
 	return r
 }
 
 func handleBlocksWatermark(log *slog.Logger, cfg HTTPServerConfig) func(w http.ResponseWriter, req *http.Request) {
-	ms := cfg.MirrorStore
 	return func(w http.ResponseWriter, req *http.Request) {
-		vh, vr, ch, cr, err := ms.NetworkHeightRound(req.Context())
+
+		currentBlock, err := cfg.GordianGRPC.GetBlocksWatermark(req.Context(), &ggrpc.CurrentBlockRequest{})
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// TODO: this should probably be an exported type somewhere.
-		var currentBlock struct {
-			VotingHeight uint64
-			VotingRound  uint32
-
-			CommittingHeight uint64
-			CommittingRound  uint32
-		}
-
-		currentBlock.VotingHeight = vh
-		currentBlock.VotingRound = vr
-		currentBlock.CommittingHeight = ch
-		currentBlock.CommittingRound = cr
-
 		if err := json.NewEncoder(w).Encode(currentBlock); err != nil {
 			log.Warn("Failed to marshal current block", "err", err)
 			return
 		}
+
 	}
 }
 
-func handleValidators(log *slog.Logger, cfg HTTPServerConfig) func(w http.ResponseWriter, req *http.Request) {
-	ms := cfg.MirrorStore
-	fs := cfg.FinalizationStore
-	reg := cfg.CryptoRegistry
-	return func(w http.ResponseWriter, req *http.Request) {
-		_, _, committingHeight, _, err := ms.NetworkHeightRound(req.Context())
-		if err != nil {
-			http.Error(
-				w,
-				fmt.Sprintf("failed to get committing height: %v", err),
-				http.StatusInternalServerError,
-			)
-			return
-		}
+// TODO:
+// func handleValidators(log *slog.Logger, cfg HTTPServerConfig) func(w http.ResponseWriter, req *http.Request) {
+// 	ms := cfg.MirrorStore
+// 	fs := cfg.FinalizationStore
+// 	reg := cfg.CryptoRegistry
+// 	return func(w http.ResponseWriter, req *http.Request) {
+// 		_, _, committingHeight, _, err := ms.NetworkHeightRound(req.Context())
+// 		if err != nil {
+// 			http.Error(
+// 				w,
+// 				fmt.Sprintf("failed to get committing height: %v", err),
+// 				http.StatusInternalServerError,
+// 			)
+// 			return
+// 		}
 
-		_, _, vals, _, err := fs.LoadFinalizationByHeight(req.Context(), committingHeight)
-		if err != nil {
-			http.Error(
-				w,
-				fmt.Sprintf("failed to load finalization: %v", err),
-				http.StatusInternalServerError,
-			)
-			return
-		}
+// 		_, _, vals, _, err := fs.LoadFinalizationByHeight(req.Context(), committingHeight)
+// 		if err != nil {
+// 			http.Error(
+// 				w,
+// 				fmt.Sprintf("failed to load finalization: %v", err),
+// 				http.StatusInternalServerError,
+// 			)
+// 			return
+// 		}
 
-		// Now we have the validators at the committing height.
-		type jsonValidator struct {
-			PubKey []byte
-			Power  uint64
-		}
-		var resp struct {
-			FinalizationHeight uint64
-			Validators         []jsonValidator
-		}
+// 		// Now we have the validators at the committing height.
+// 		type jsonValidator struct {
+// 			PubKey []byte
+// 			Power  uint64
+// 		}
+// 		var resp struct {
+// 			FinalizationHeight uint64
+// 			Validators         []jsonValidator
+// 		}
 
-		resp.FinalizationHeight = committingHeight
-		resp.Validators = make([]jsonValidator, len(vals))
-		for i, v := range vals {
-			resp.Validators[i].Power = v.Power
-			resp.Validators[i].PubKey = reg.Marshal(v.PubKey)
-		}
+// 		resp.FinalizationHeight = committingHeight
+// 		resp.Validators = make([]jsonValidator, len(vals))
+// 		for i, v := range vals {
+// 			resp.Validators[i].Power = v.Power
+// 			resp.Validators[i].PubKey = reg.Marshal(v.PubKey)
+// 		}
 
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			log.Warn("Failed to marshal validators response", "err", err)
-			return
-		}
-	}
-}
+// 		if err := json.NewEncoder(w).Encode(resp); err != nil {
+// 			log.Warn("Failed to marshal validators response", "err", err)
+// 			return
+// 		}
+// 	}
+// }
