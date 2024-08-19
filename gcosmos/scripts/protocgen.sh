36e533ed8f54143c63a5d53b0a21879b46869a39
Reece Williams <reecepbcups@gmail.com>
2024-08-19 11:40:49 -0500

    Revert "gRPC: rust client example w/ tonic"
    This reverts commit 07caec6ac753a841012dd6dac0be26c473f8f91c.

--- gcosmos/scripts/protocgen.sh
+++ gcosmos/scripts/protocgen.sh
@@ -1,16 +1,6 @@
 # protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/**/*.proto
 protoc --go_out=. --go-grpc_out=. proto/**/*.proto
 
-
-# cargo install protoc-gen-tonic
-mkdir -p rust
-touch rust/server.rs
-protoc -I proto --tonic_out=rust proto/**/*.proto
-# protoc --rust_out=experimental-codegen=enabled,kernel=cpp:. proto/**/*.proto
-
-# move all .rs and .cc files from within proto -> the rust/ dir
-mv proto/**/*.rs proto/**/*.cc rust/
-
 cp ./github.com/rollchains/gordian/gcosmos/gserver/internal/grpc/* ./gserver/internal/grpc
 rm -rf ./github.com
 
