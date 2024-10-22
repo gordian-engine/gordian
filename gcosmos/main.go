185afc452f9251e4e699aa366808545a7f8f9a1e
Mark Rushakoff <mark.rushakoff@gmail.com>
2024-10-22 16:02:30 -0400

    chore: rename github org to gordian-engine
    The gordian repo is moving to that org.
    
    Did a global find and replace with:
    
        export CUR="github.com/rollchains/gordian"
        export NEW="github.com/gordian-engine/gordian"
        go mod edit -module "${NEW}"
        find . -type f -name '*.go' -exec perl -pi -e 's/$ENV{CUR}/$ENV{NEW}/g' {} \;
    
    Then I needed to update go.mod for tmsqlite and gcosmos, and then I ran
    go fmt ./... in each module.

--- gcosmos/main.go
+++ gcosmos/main.go
@@ -12,10 +12,9 @@ import (
 	"cosmossdk.io/simapp/v2"
 	simdcmd "cosmossdk.io/simapp/v2/simdv2/cmd"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	"github.com/rollchains/gordian/gcosmos/internal/gci"
+	"github.com/gordian-engine/gordian/gcosmos/internal/gci"
 	"github.com/spf13/cobra"
 )
-
 
 func main() {
 	var rootCmd *cobra.Command
