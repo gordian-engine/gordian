6fab8c1eb2868abf4bae2b2ee9601f640d152e3f
Mark Rushakoff <mark.rushakoff@gmail.com>
2024-12-10 16:54:03 -0500

    feat: add gerasure package
    This package enables using erasure coding to translate a slice of data
    into erasure-coded shards that can be used to reconstitute the original
    data. The first implementation is an off-the-shelf Reed-Solomon coding
    library (which uses shards of the original data and separate parity
    shards).
    
    The interface should also enable rateless erasure coding, which shards
    the data differently, should that use case come up.

---
+++ gerasure/coding.go
@@ -0,0 +1,52 @@
+package gerasure
+
+import (
+	"context"
+	"errors"
+)
+
+// Encoder encodes data into a set of erasure-corrected shards of bytes.
+// The precise set of byte slices returned,
+// and which of them are required to reconstitute the original data,
+// are determined by the implementation.
+type Encoder interface {
+	Encode(ctx context.Context, data []byte) ([][]byte, error)
+}
+
+// Reconstructor manages reconstructing the original data
+// from a series of erasure-coded shards,
+// typically produced from an instance of [Encoder].
+//
+// This interface currently is focused on reconstructing only the data shards.
+// In the future, we may expand the interface with a new method
+// to repopulate any missing parity shards, if that proves useful.
+type Reconstructor interface {
+	// Reconstruct attempts to use the shard to reconstruct the original data.
+	//
+	// Some erasure coding schemes (such as Reed Solomon) have an explicit index for each shard;
+	// "rateless" erasure codes may ignore the index or may require that each call
+	// uses an incremented index.
+	//
+	// If the shard was accepted properly and the data is able to be reconstructed,
+	// Reconstruct returns nil.
+	// If the shard was accepted but more shards are required,
+	// Reconstruct returns [ErrIncompleteSet].
+	// Otherwise, an implementation-specific error is returned.
+	ReconstructData(ctx context.Context, idx int, shard []byte) error
+
+	// Data appends the reconstructed data to dst,
+	// returning the modified dst slice if dst has sufficient capacity,
+	// otherwise returning a newly allocated slice.
+	//
+	// We are assuming for now that all erasure-coded data encountered will fit in memory;
+	// if that assumption changes, we may add a new method to this interface
+	// or add a separate interface altogether.
+	//
+	// Any error in producing the data may be returned directly,
+	// with no wrapping by any gerasure errors.
+	Data(dst []byte, dataSize int) ([]byte, error)
+}
+
+// ErrIncompleteSet is returned by [Reconstructor.Restore] when a shard was accepted
+// but was not sufficient to fully restore the original data.
+var ErrIncompleteSet = errors.New("insufficient shard received to reconstruct data")
