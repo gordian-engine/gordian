68b99374e901b6d9388463f1a174335bb989f7e6
Mark Rushakoff <mark.rushakoff@gmail.com>
2024-12-17 16:45:24 -0500

    feat: initial sketch of BLS
    This first implementation uses the minimized-signature form. I don't
    think we have a use case for the minimized-key form, but the packages
    are structured such that we can add it later.
    
    I don't think this internal API is finalized yet; we will need to try it
    out with the existing gcrypto signature proof interfaces to see what
    refining it needs.
    
    We may need some adjustments to account for the salt. I need to double
    check whether we need any further custom data for the Domain Separation
    Tag. Using the compressed forms of points is useful for serialization,
    but we may want separate methods to expose the raw points, in order to
    more efficiently aggregate keys and signatures.
    
    There is an outstanding issue on the blst repository about compatibility
    with go1.24, so the dependency on blst means that for now, using BLS
    means sticking with go1.23.

---
+++ gcrypto/gbls/gblsminsig/doc.go
@@ -0,0 +1,14 @@
+// Package gblsminsig wraps [github.com/supranational/blst/bindings/go]
+// to provide a [gcrypto.PubKey] implementation backed by BLS keys,
+// where the BLS keys have minimized signatures.
+//
+// The blst dependency requires CGo,
+// so therefore this package also requires CGo.
+//
+// Two key references for correctly understanding and using BLS keys are
+// [RFC9380] (Hashing to Elliptic Curves)
+// and the IETF draft for [BLS Signatures].
+//
+// [RFC9380]: https://www.rfc-editor.org/rfc/rfc9380.html
+// [BLS Signatures]: https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-05
+package gblsminsig
